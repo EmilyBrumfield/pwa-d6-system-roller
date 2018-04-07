ef595f3421a1d5619393a102c9e90b7cd4a0c11b
EmilyBrumfield <belac54@hotmail.com>
2018-04-07 11:49:44 -0400

    test2

--- sw.js
+++ sw.js
@@ -1,53 +1,30 @@
-//Used Google's Air Horner example as a basis for this serviceworker
-//I have modified this file.
+importScripts('/cache-polyfill.js');
 
-/*
- *
- *  Air Horner
- *  Copyright 2015 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
-
-// Version 0.6.5
-self.addEventListener('install', e => {
-  const timeStamp = Date.now();
-  e.waitUntil(
-    caches.open('d6roller').then(cache => {
-      return cache.addAll([
-/*
-        `/`,
-        `/index.html?timestamp=${timeStamp}`,
-        `/index.css?timestamp=${timeStamp}`,
-        `/index.js?timestamp=${timeStamp}`,
-        `/genericIcon.ico?timestamp=${timeStamp}`,
-        `/placeholderIcon.ico?timestamp=${timeStamp}`,
-*/
-      ])
-          .then(() => self.skipWaiting());
-    })
-  );
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('d6roller').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.css',
+       '/index.js',
+     ]);
+   })
+ );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
+self.addEventListener('fetch', function(event) {
 
-self.addEventListener('fetch', event => {
+  console.log(event.request.url);
+  
   event.respondWith(
-    caches.match(event.request, {ignoreSearch: true}).then(response => {
-      return response || fetch(event.request);
-    })
+  
+  caches.match(event.request).then(function(response) {
+  
+  return response || fetch(event.request);
+  
+  })
+  
   );
-});
+  
+  });
