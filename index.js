361a70d22d847d847bd7719658268a76b6380633
EmilyBrumfield <belac54@hotmail.com>
2018-04-07 11:30:41 -0400

    test serviceworker

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 // ServiceWorker is a progressive technology. Ignore unsupported browsers
-if ('serviceWorker' in navigator) {
-  console.log('CLIENT: service worker registration in progress.');
-  navigator.serviceWorker.register('service-worker.js').then(function() {
-    console.log('CLIENT: service worker registration complete.');
-  }, function() {
-    console.log('CLIENT: service worker registration failure.');
-  });
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+           .register('/sw.js')
+           .then(function() { console.log("Service Worker Registered"); });
 } else {
   console.log('CLIENT: service worker is not supported.');
 }
