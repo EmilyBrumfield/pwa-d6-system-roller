b255aa2881339065b8579c100e548d13abbc94b9
EmilyBrumfield <belac54@hotmail.com>
2018-04-08 06:38:17 -0400

    test4

--- sw.js
+++ sw.js
@@ -12,14 +12,23 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('fetch', function(event) {
-
   console.log(event.request.url);
-  
   event.respondWith(
-  
   caches.match(event.request).then(function(response) {
+    if (response) {
+      return response || fetch(event.request);
+    } else {
+      return fetch(event.request)
+        .then(function(res) {
+          return caches.open('d6roller')
+            .then(function(cache) {
+              cache.put(event.request.url, res.clone());
+              return res;
+            })
+        })
+        
+    }
   
-  return response || fetch(event.request);
   
   })
   
